0a122d51c44e735a76f1533b512c3787cd6b067a
Mohammad Ehson <30387730+ehsonmiraz@users.noreply.github.com>
2022-10-15 10:49:53 -0700

    Update sum_of_power.c

--- sum_of_power.c
+++ sum_of_power.c
@@ -3,11 +3,12 @@
 void main()
 {
     printf("RameshwarSingh\n");
+    
     int n, sum = 0, i;
     printf("Enter power of values whose sum is needed to be found : ");
     scanf("%d", &n);
     for (i = 1; i <=10; i++)
         sum = sum + pow(i,n);
-    printf("Sum of first 10 number  power %d is %d\n", n, sum);
+    printf("Sum of first 10 number to the power %d is %d\n", n, sum);
 }
 // This code was edited by Rameshwar Singh
